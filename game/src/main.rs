525c19697e6bd453f862361753ab3ba990f0a177
VictoremWinbringer <victor@mail.ru>
2018-10-28 00:21:20 +0300

    save PointWriter

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,109 +1,140 @@
+extern crate termion;
+extern crate term;
 
+use term::stdout;
+use termion::raw::IntoRawMode;
+use std::io::Write;
 
-//Entities
+//Entities ------------------------------------------------------------------
 
-#[derive(Debug,Clone)]
+#[derive(Debug, Clone, Eq, PartialEq, Default)]
 struct Point {
-    x:u8,
-    y:u8
+    x: u8,
+    y: u8,
 }
 
 impl Point {
-    pub fn intersects(&self, point:&Point) -> bool {
+    pub fn intersects(&self, point: &Point) -> bool {
         self.x == point.x && self.y == point.y
     }
 }
 
+#[derive(Debug, Clone, Eq, PartialEq, Default)]
 struct Frame {
     min_x: u8,
     min_y: u8,
     max_x: u8,
-    max_y: u8
+    max_y: u8,
 }
 
 impl Frame {
-    pub fn intersects(&self, point:&Point) -> bool {
+    pub fn intersects(&self, point: &Point) -> bool {
         point.x == self.min_x
-        || point.y == self.min_y
-        || point.x == self.max_x
-        || point.y == self.max_y
+            || point.y == self.min_y
+            || point.x == self.max_x
+            || point.y == self.max_y
     }
 }
 
+#[derive(Debug, Clone, Eq, PartialEq)]
 enum Direction {
     Left,
     Right,
     Top,
-    Bottom
+    Bottom,
 }
 
+impl Default for Direction {
+    fn default() -> Direction {
+        return Direction::Right;
+    }
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Default)]
 struct Snake {
     direction: Direction,
     points: std::collections::VecDeque<Point>,
-    start_x:u8,
-    start_y:u8,
+    start_x: u8,
+    start_y: u8,
 }
 
 impl Snake {
-    pub fn new(x:u8, y:u8) -> Snake {
+    pub fn new(x: u8, y: u8) -> Snake {
         let mut points = std::collections::VecDeque::new();
         for i in 0..3 {
-            points.push_front(Point{x:x+i,y: i + y });
+            points.push_front(Point { x: x + i, y: i + y });
         }
-        Snake{direction:Direction::Right, points, start_x:x,start_y:y}
+        Snake { direction: Direction::default(), points, start_x: x, start_y: y }
     }
 
     pub fn grow(mut self) -> Snake {
         if let Some(tail) = self.points.pop_back() {
-            self.points.push_back(Point{x:tail.x,y:tail.y});
+            self.points.push_back(Point { x: tail.x, y: tail.y });
             self.points.push_back(tail);
         }
         self
     }
 
-    pub fn reset(self) -> Snake{
-         Snake::new(self.start_x,self.start_y)
+    pub fn reset(self) -> Snake {
+        Snake::new(self.start_x, self.start_y)
     }
 
-    pub fn turn(mut self, direction:Direction) -> Snake {
+    pub fn turn(mut self, direction: Direction) -> Snake {
         self.direction = direction;
         self
     }
 
-    pub fn try_eat(mut self, point:&Point) -> Snake {
-            if let Some(head) = self.points.pop_front() {
-                let h = head.clone();
-                self.points.push_front(h);
-                if head.intersects(point) {
-                 return  self.grow();
-                }
-            }
+    pub fn try_eat(mut self, point: &Point) -> Snake {
+        let head = self.head();
+        if head.intersects(point) {
+            return self.grow();
+        }
         self
     }
 
-    pub fn try_intersect_frame(mut self, frame:&Frame) -> Snake {
-        if let Some(head) = self.points.pop_front() {
-            let h = head.clone();
-            self.points.push_front(h);
-            if frame.intersects(&head) {
-              return   self.reset()
-            }
+    pub fn try_intersect_frame(mut self, frame: &Frame) -> Snake {
+        let head = self.head();
+        if frame.intersects(&head) {
+            return self.reset();
         }
         self
     }
 
     pub fn try_intersect_tali(mut self) -> Snake {
-        if let Some(head) = self.points.pop_front() {
-            let h = head.clone();
-            self.points.push_front(h);
-            let p = self.points.clone();
-            let points = p.into_iter().filter(|p| head.intersects(p));
-            if points.count() > 1 {
-              return self.reset()
-            }
+        let head = self.head();
+        let p = self.points.clone();
+        let points = p.into_iter().filter(|p| head.intersects(p));
+        if points.count() > 1 {
+            return self.reset();
         }
         self
     }
+
+    pub fn head(&self) -> Point {
+        self.points.front().unwrap().clone()
+    }
+}
+
+//Data Access Layer ----------------------------------------------------------------
+
+struct PointWriter {
+    symbol:char
+}
+
+impl PointWriter {
+    pub fn new(symbol: char) -> PointWriter {
+        PointWriter{symbol}
+    }
+    pub fn write(&self, point:&Point) {
+        let mut stdout = stdout().into_raw_mode().unwrap();
+        write!(stdout, "{}{}", termion::cursor::Goto(point.x.into(),point.y.into()),self.symbol)
+            .unwrap();
+        stdout.flush().unwrap();
+    }
+}
+
+struct FrameWriter{
+    symbol:char
 }
 
 fn main() {
--- game/src/tests.rs
+++ game/src/tests.rs
@@ -26,3 +26,98 @@ fn test_frame_intersects() {
     assert_eq!(false, f1.intersects(&p2));
 }
 
+#[test]
+fn test_snake_new(){
+    let snake = Snake::new(1,2);
+    assert_eq!(1, snake.start_x);
+    assert_eq!(2, snake.start_y);
+    assert_eq!(Direction::Right, snake.direction);
+}
+
+#[test]
+fn test_snake_grow(){
+    let snake = Snake::new(1,2);
+    let old = snake.points.clone();
+    let newSnake = snake.grow();
+    let new = newSnake.points.clone();
+    assert_eq!(1, new.len() - old.len());
+}
+
+#[test]
+fn test_snake_reset(){
+    let snake = Snake::new(1,2).grow().reset();
+    let snake2 = Snake::new(1,2);
+    assert_eq!(snake,snake2);
+}
+
+#[test]
+fn test_snake_turn() {
+let snake = Snake::new(1,2);
+    let snake2 = Snake::new(1,2).turn(Direction::Top);
+    assert_eq!(snake.direction, Direction::default());
+    assert_eq!(snake2.direction, Direction::Top);
+    assert_ne!(snake2.direction, snake.direction);
+}
+
+#[test]
+fn test_snake_try_eat(){
+    let snake = Snake::new(1,2);
+    let point = snake.head();
+    let point2 = Point{x:100, y:100};
+
+    let len1 = snake.points.len();
+    let snake = snake.try_eat(&point2);
+    let len2 = snake.points.len();
+    let len3 = snake.try_eat(&point).points.len();
+    assert_eq!(len1,len2);
+    assert_eq!(1, len3 - len2);
+}
+
+#[test]
+fn test_snake_try_intersect_frame_true(){
+    let snake = Snake::new(1,2).grow();
+    let snake2 = Snake::new(1,2);
+    let head = snake.head();
+    let frame = Frame{
+        min_x: 1,
+        min_y: 1,
+        max_x: head.x,
+        max_y: head.y
+    };
+    let snake = snake.try_intersect_frame(&frame);
+    assert_eq!(snake,snake2);
+}
+
+#[test]
+fn test_snake_try_intersect_frame_false(){
+    let snake = Snake::new(1,2).grow();
+    let snake2 = Snake::new(1,2);
+    let frame = Frame{
+        min_x: 1,
+        min_y: 1,
+        max_x: 255,
+        max_y: 255
+    };
+    let snake = snake.try_intersect_frame(&frame);
+    assert_ne!(snake,snake2);
+    assert_eq!(1, snake.points.len() - snake2.points.len());
+}
+
+#[test]
+fn test_try_intersect_tail_true(){
+    let mut snake = Snake::new(1,2).grow();
+    let snake2 = Snake::new(1,2);
+    let head = snake.head();
+    snake.points.push_back(head);
+    let snake = snake.try_intersect_tali();
+    assert_eq!(snake, snake2);
+}
+
+#[test]
+fn test_try_intersect_tail_false(){
+    let mut snake = Snake::new(1,2).grow();
+    let snake2 = Snake::new(1,2);
+    let snake = snake.try_intersect_tali();
+    assert_ne!(snake,snake2);
+    assert_eq!(1, snake.points.len() - snake2.points.len());
+}
